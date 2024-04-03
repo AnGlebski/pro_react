0eb2ab962e2b7d86168fb00aeca312af5a06127f
AnGlebski <gluhova19962010@mail.ru>
2024-04-03 21:05:22 +0300

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import App from './App'
+import { BrowserRouter } from 'react-router-dom'
+import { render } from 'react-dom'
+import ThemeProvider from './theme/ThemeProvider'
+render(
+  <BrowserRouter>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </BrowserRouter>,
+
+  document.getElementById('root')
+)
